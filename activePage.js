32eb673e7463f40866955d1940ca15887f318ef6
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2023-04-08 15:41:16 -0300

    change folder structure for deployment, fix pages urls paths and add styles

--- activePage.js
+++ activePage.js
@@ -1,5 +1,4 @@
 const activePage = window.location.pathname;
-// console.log(activePage);
 
 const navLinks = document.querySelectorAll("nav a").forEach((link) => {
   if (link.href.includes(`${activePage}`)) {
